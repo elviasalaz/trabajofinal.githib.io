790732010f8bba073e56169889965f57cd66f847
elviasalaz <118481398+elviasalaz@users.noreply.github.com>
2022-12-15 21:06:15 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+        
+    
+    let zona = confirm("Vives en San Nicolas?");
+        if (zona ===true) {
+            alert("Eres Nicoleño!!, Gracias por comprar mis productos, tu envio es gratis");
+
+        }else{
+            alert("Disculpa, tu ubicación se encuentra muy lejos, tu envio tiene un costo de $800");
+        }
